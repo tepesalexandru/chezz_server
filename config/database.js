eff1ecd40d4b1e4b79410130bd63f1ef9a9f6a25
Tepes Alexandru <godzplayer@hotmail.com>
2020-10-11 21:40:32 +0300

    Update database config

--- config/database.js
+++ config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
+        client: "postgres",
+        host: "${process.env.DATABASE_HOST}",
+        port: "${process.env.DATABASE_PORT}",
+        database: "${process.env.DATABASE_NAME}",
+        username: "${process.env.DATABASE_USERNAME}",
+        password: "${process.env.DATABASE_PASSWORD}",
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {}
     },
   },
 });
